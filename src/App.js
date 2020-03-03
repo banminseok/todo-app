886badb612790c040d6220b4c618a476c45fe9fe
banminseok <30819583+banminseok@users.noreply.github.com>
2020-03-03 13:39:56 +0900

    works 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <div>Todo App 를 만들자!</div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리엑트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정관리 앱 만들어보기',
+      checked: false,
+    },
+  ]);
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
 };
 
 export default App;
