27ba73c1db5b13ee4d840c7fa711d2384e09b7b8
banminseok <30819583+banminseok@users.noreply.github.com>
2020-03-06 22:42:42 +0900

    react-vitualized test

--- src/App.js
+++ src/App.js
@@ -2,68 +2,65 @@ import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { dispatch } from '../../../Users/반민석/AppData/Local/Microsoft/TypeScript/3.7/node_modules/rxjs/internal/observable/pairs';
 
-function createBulkTodos(){
-  const array =[];
-  for (let i=1;i<=2500;i++){
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
     array.push({
-      id:i,
+      id: i,
       text: `할 일 ${i}`,
-      checked:false,
+      checked: false,
     });
   }
   return array;
 }
-function todoReducer (todos, action){
-  switch (action.type){
-    case 'INSERT' :
-    // {type:insert , todo:{id:1, text:todo,checked :false}}
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      // {type:insert , todo:{id:1, text:todo,checked :false}}
       return todos.concat(action.todo);
-    case 'REMOVE' :
-      return todos.filter(todo=>todo.id !== action.id);
-    case 'TOGGLE' :
-      return todos.map(todo => todo.id === action.id ? {...todo, checked:!todo.checked}:todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
     default:
       return todos;
-  }//
+  } //
 }
 const App = () => {
   const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos => todos.concat(todo));
-      nextId.current += 1;
-    },
-    []
-  );
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    id=>{
-      setTodos(todos => todos.filter(todo=>todo.id !== id));
-    },[]
-  );
+  const onRemove = useCallback(id => {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }, []);
 
   const onToggle = useCallback(
     /*id =>{
       setTodos(todos=>todos.map(todo=>todo.id===id ?{...todo, checked:!todo.checked} :todo));
     },[]*/
-    id =>{
-      setTodos(
-        todos=>todos.map(
-          //todo => todo.id ===id ? {...todo, checked: !todo.checked}:todo,   
-          todo=> todo.id ===id ? {...todo, checked: !todo.checked}:todo,
+    id => {
+      setTodos(todos =>
+        todos.map(
+          //todo => todo.id ===id ? {...todo, checked: !todo.checked}:todo,
+          todo => (todo.id === id ? { ...todo, checked: !todo.checked } : todo),
         ),
-      );      
-    },[]
+      );
+    },
+    [],
   );
   return (
     <TodoTemplate>
